298aab6dad5ea8e12228b96b27ce826509fb98ab
Gunwoo Kim <gunwoo@gunwoo.org>
2018-08-29 22:53:43 +1000

    Add errors for user migration Lambda

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"errors"
+)
+
 const (
 	// CognitoTriggerSourceUserMigrationAuthentication triggers a user migration at the time of sign in.
 	CognitoTriggerSourceUserMigrationAuthentication = "UserMigration_Authentication"
 	// CognitoTriggerSourceUserMigrationForgotPassword triggers a user migration during forgot-password flow.
 	CognitoTriggerSourceUserMigrationForgotPassword = "UserMigration_ForgotPassword"
+)
+
+var (
+	// ErrBadPassword is thrown if the password entered by the user for sign-in is wrong.
+	ErrBadPassword = errors.New("Bad password")
+	// ErrBadTriggerSource is thrown when the specified trigger source is not supported.
+	ErrBadTriggerSource = errors.New("Bad trigger source")
 )

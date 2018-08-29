87d574fee72759e23e1c231b213bb478fa1d5e4b
Gunwoo Ben Kim <gunwoo@gunwoo.org>
2018-08-29 22:13:09 +1000

    Add trigger sources for user migration Lambda

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	// CognitoTriggerSourceUserMigrationAuthentication triggers a user migration at the time of sign in.
+	CognitoTriggerSourceUserMigrationAuthentication = "UserMigration_Authentication"
+	// CognitoTriggerSourceUserMigrationForgotPassword triggers a user migration during forgot-password flow.
+	CognitoTriggerSourceUserMigrationForgotPassword = "UserMigration_ForgotPassword"
+)

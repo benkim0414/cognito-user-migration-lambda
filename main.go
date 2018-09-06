855891466e5120e9ced927797ac072b1d91c431f
Gunwoo Ben Kim <gunwoo@gunwoo.org>
2018-09-06 20:51:19 +1000

    Implement user migration handler

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 const (
@@ -17,3 +19,40 @@ var (
 	// ErrBadTriggerSource is thrown when the specified trigger source is not supported.
 	ErrBadTriggerSource = errors.New("Bad trigger source")
 )
+
+// Handler migrates the user with an existing password and suppresses the welcome message from Amazon Cognito.
+func Handler(event CognitoEventUserPoolsMigrateUser) (CognitoEventUserPoolsMigrateUser, error) {
+	switch event.TriggerSource {
+	case CognitoTriggerSourceUserMigrationAuthentication:
+		// Authenticate the user with existing user directory service
+		user, err := authenticateUser(event.UserName, event.Request.Password)
+		if err != nil {
+			return CognitoEventUserPoolsMigrateUser{}, err
+		}
+		event.Response.UserAttributes = map[string]string{
+			"email":          user.email,
+			"email_verified": "true",
+		}
+		event.Response.FinalUserStatus = "CONFIRMED"
+		event.Response.MessageAction = "SUPPRESS"
+		return event, nil
+	case CognitoTriggerSourceUserMigrationForgotPassword:
+		// Lookup the user in existing user directory service
+		user, err := lookupUser(event.UserName)
+		if err != nil {
+			return CognitoEventUserPoolsMigrateUser{}, err
+		}
+		event.Response.UserAttributes = map[string]string{
+			"email":          user.email,
+			"email_verified": "true",
+		}
+		event.Response.MessageAction = "SUPPRESS"
+		return event, nil
+	default:
+		return CognitoEventUserPoolsMigrateUser{}, ErrBadTriggerSource
+	}
+}
+
+func main() {
+	lambda.Start(Handler)
+}
